f9702b37bd294c0927a4b7f9ab8aad215ca1f1aa
spinweb1 <spinweb1@yahoo.com>
2016-08-22 09:53:19 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,208 @@
+// Model is an object literal
+var model = {
+	selectedCat: null,
+	adminVis: null,
+	cats: [		
+		{
+			name: 'William',
+			clicks: 0,
+			image: 'img/hider.jpg',
+		},
+		{
+			name: 'Stealthy',
+			clicks: 0,
+			image: 'img/sneaky.jpg'
+		},
+		{
+			name: 'Siamese',
+			clicks: 0,
+			image: 'img/siamese.jpg'
+		},
+		{
+			name: 'Dixie',
+			clicks: 0,
+			image: 'img/buzz.jpg'
+		},
+		{
+			name: 'Shortie',
+			clicks: 0,
+			image: 'img/shortie.jpg'
+		}	
+	]
+};
+
+
+
+var octopus = {
+
+	init: function(){
+        // Set our current cat to the first one in the list
+		model.selectedCat = model.cats[0];
+		//model.adminVis = false;
+		
+		// Initialize views
+		catListView.init();
+		catView.init();
+		adminView.init();
+	},
+
+	getAllCats: function(){
+		return model.cats;
+	},
+
+	getSelectedCat: function(){
+		return model.selectedCat;
+	},
+	
+	// Set the currently-selected cat to the object passed in
+	setCurrentCat: function(cat){
+		model.selectedCat = cat;
+	},
+	
+	// Increments the counter for the currently-selected cat
+	clickCounter: function(){
+		model.selectedCat.clicks++;
+		catView.render();
+	},
+	
+	showAdmin: function(){
+		model.adminVis = true;
+		adminView.render();
+	},
+	
+	hideAdmin: function(){
+		model.adminVis = false;
+	},
+	
+	saveAdmin: function(){
+		setCurrentCat();
+
+		
+	}
+		
+};
+
+
+
+var catListView = {  
+
+	init: function(){
+		// store the DOM element for easy access later
+		this.catListElem = document.getElementById('cat-list');
+
+        // render this view (update the DOM elements with the right values)
+		this.render();
+	},
+
+	render: function(){
+		
+		var cat, elem, i;
+		// Get the cats we'll be rendering from the octopus (which gets it from the model)
+		var cats = octopus.getAllCats();
+		
+		// Clear out text
+		this.catListElem.innerHTML = '';
+
+		// Let's loop over the cats in our array
+		for (i = 0; i < cats.length; i++){
+
+			// This is the number we're currently on...
+			var cat = cats[i];
+
+			// Make a cat list item and set its text
+			elem = document.createElement('li');
+			elem.textContent = cat.name;
+			
+			// On click, setCurrentCat and render the catView
+			// (this uses our closure-in-a-loop trick to connect the value
+			// of the cat variable to the click event function)
+			elem.addEventListener('click', (function(catCopy){
+				return function(){
+					octopus.setCurrentCat(catCopy);
+					catView.render();
+				};
+			})(cat));
+			
+			// Finally, add the element to the list
+			this.catListElem.appendChild(elem);
+		}
+	}	
+};
+
+var catView = {
+	// Render gets called over and over, but init just once
+	
+	init: function(){
+		// Store pointers to our DOM elements for easy access later
+		this.catElem = document.getElementById('cat');
+		this.catNameElem = document.getElementById('cat-name');
+		this.catImageElem = document.getElementById('cat-img');
+		this.countElem = document.getElementById('cat-count');
+		this.imgUrl = document.getElementById('img-url');
+		
+		// On click, increment the current cat's counter 
+		// Only need to do once, values are changed
+		this.catImageElem.addEventListener('click', function(){
+			octopus.clickCounter();
+		});
+		
+		//  Render this view (update the DOM elements with the right values)
+		this.render();	
+	},
+	
+	render: function(){	
+		// Update the DOM elements with values from the current cat
+		var selectedCat = octopus.getSelectedCat(); // (which gets it from the model)
+		this.countElem.textContent = selectedCat.clicks;
+		this.catNameElem.textContent = selectedCat.name;
+		this.catImageElem.src = selectedCat.image;
+		this.imgUrl.textContent = "The path to this image is " + selectedCat.image;
+	}
+};
+
+var adminView = {
+	
+	init: function(){
+		// Store pointers to our DOM elements for easy access later	
+		var selectedCat = octopus.getSelectedCat();	
+		this.name = document.getElementById('cat-name');
+		this.adminImgUrl = document.getElementById('cat-img');
+		this.numClicks = catView.countElem;
+		this.saveBtn = document.getElementById('save');
+		this.cancelBtn = document.getElementById('cancel');
+		this.adminBtn = document.getElementById('admin');
+		this.adminVis  = document.getElementById("adminSec").style.visibility = "hidden";
+		
+		this.cancelBtn.addEventListener('click', function(){
+			octopus.hideAdmin();
+		});
+		
+		this.adminBtn.addEventListener('click', function(){
+			console.log(selectedCat.name);
+			octopus.showAdmin();
+		});
+		
+		this.saveBtn.addEventListener('click', function(){
+			octopus.saveAdmin();
+			alert("saveAdmin called");
+		});	
+		
+	},
+	
+	render: function(){
+		var selectedCat = octopus.getSelectedCat(); // (which gets it from the model)
+		this.adminVis = document.getElementById("adminSec").style.visibility = "visible";
+		
+		this.numClicks.textContent = document.getElementById('cat-count');
+		this.name.textContent = selectedCat.name;
+		this.adminImgUrl.textContent = catView.image;
+		
+		//  Render this view (update the DOM elements with the right values)
+		//this.render();
+
+	}
+	
+};
+
+// Make it go!
+octopus.init();
